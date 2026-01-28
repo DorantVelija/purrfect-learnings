b2a56bede2738186889ce687b9dd7f3df7423d93
DorantVelija <dorantvelija@outlook.com>
2026-01-28 21:03:42 +0100

    add authorization input thingy on swagger

--- backend/Purrfect-Learnings/Program.cs
+++ backend/Purrfect-Learnings/Program.cs
@@ -4,13 +4,46 @@ using Purrfect_Learnings.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Purrfect Learnings API",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter: Bearer {your JWT token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Configure PostgreSQL
 builder.Services.AddDbContext<AppDbContext>(options =>
--- backend/Purrfect-Learnings/obj/Debug/net9.0/Purrfect-Learnings.AssemblyInfo.cs
+++ backend/Purrfect-Learnings/obj/Debug/net9.0/Purrfect-Learnings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Purrfect-Learnings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba8411c0c3508d12228e4d4ac4961d929bfe88f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0870aeefe5a4632f5a35978b75b17cfe0e5007f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Purrfect-Learnings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Purrfect-Learnings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
